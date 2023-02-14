32bf52fb76cfb07644704cbeeed7c98c946501b5
Lucas Garza <52394488+pidginenglish@users.noreply.github.com>
2023-02-13 18:56:09 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.date;
+
 public class Main {
     public static void main(String[] args) {
+        Date date = new Date();
         System.out.println("Hello world!");
+        System.out.println("Today's date: " + date);
     }
 }
